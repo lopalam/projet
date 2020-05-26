ebe963b7091103c87b2f736b37bedddf2660310b
Samuel Adone <syadone@gmail.com>
2020-05-26 15:57:24 +0200

    Linted the whole file

--- index.php
+++ index.php
@@ -4,11 +4,13 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Accueil</title>
 </head>
+
 <body>
     <header>
         <span ID="title_span">
@@ -16,15 +18,16 @@ session_start();
             <h2>Bienvenu <?php $_SESSION['prenom'] ?></h2>
         </span>
         <span ID="log_span">
-            <form ID="log_in_button" action="log_in.html"> 
+            <form ID="log_in_button" action="log_in.html">
                 <button type="submit">Connexion</button>
             </form>
-            <form ID="sign_up_button" action="sign_up.html"> 
+            <form ID="sign_up_button" action="sign_up.html">
                 <button type="submit">S'enregistrer</button>
             </form>
         </span>
         <hr>
     </header>
-    
+
 </body>
+
 </html>
