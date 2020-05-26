d1c5839e0531aec2ab87b214aac5d925f0f64a2a
lopalam <Valentin.lachamp@ynov.com>
2020-05-26 15:54:02 +0200

    gestion button

--- index.php
+++ index.php
@@ -12,24 +12,33 @@ session_start();
 <body>
     <header>
         <span ID="title_span">
-            <h1>Titre principal</h1>
+            <h1>Docto-covid-allo</h1>
             <?php if (isset($_SESSION['IS_CONNECTED'])) {
                 ?><h2>Bienvenu <?php $_SESSION['donnees']['prenom']?> </h2><?php
             }    
             ?>
         </span>
-        <span ID="log_span">
+        <span ID="button_span">
+            <?php if (isset($_SESSION['IS_CONNECTED'])==false) {
+            ?>
             <form ID="log_in_button" action="log_in.html"> 
                 <button type="submit">Connexion</button>
             </form>
             <form ID="sign_up_button" action="sign_up.html"> 
                 <button type="submit">S'enregistrer</button>
             </form>
+            <?php
+            }    
+            ?>
             <?php if (isset($_SESSION['IS_CONNECTED'])) {
                 ?>
             <form ID="log_out_button" action="log_out.php">
                 <button type="submit">Déconnexion</button>
-            </form><?php
+            </form>
+            <form ID="profile_button" action="profil.php">
+                <button type="submit">Modifier votre profil</button>
+            </form>
+            <?php
             }    
             ?>
         </span>
---
+++ profil.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil</title>
+</head>
+<body>
+    <header>
+        <span ID="title_span">
+            <h1>Docto-covid-allo</h1>
+        </span>
+        <hr>
+    </header>
+</body>
+</html>
