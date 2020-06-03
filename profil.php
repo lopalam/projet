50d6d9428d3cced89046de0815b816a8e2fb8c78
Samuel Adone <syadone@gmail.com>
2020-06-03 14:16:32 +0200

    prettier

--- profil.php
+++ profil.php
@@ -22,7 +22,7 @@ session_start();
         <form action="save_profil.php" method="post">
         <button type="submit">Sauvegarder</button>
 
-        <ul>
+        <ul class="profil">
             <li> Prénom (<?php print_r($_SESSION['donnees']['prenom']) ?>):  <input type="text" name="prenom" placeholder="Nouveau prénom" /> </li>
             <li> Nom (<?php print_r($_SESSION['donnees']['nom']) ?>):  <input type="text" name="nom" placeholder="Nouveau nom" /> </li>
             <li> Email (<?php print_r($_SESSION['donnees']['email']) ?>):  <input type="text" name="email" placeholder="Nouvelle adresse mail" /> </li>
