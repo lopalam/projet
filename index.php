a6e72e9244fdf31c0ca8ad6205e8020ff6ec3262
lopalam <Valentin.lachamp@ynov.com>
2020-06-09 15:44:14 +0200

    update index.php

--- index.php
+++ index.php
@@ -26,27 +26,22 @@ if (isset($_SESSION['IS_CONNECTED']) == false) {
             ?>
         </span>
         <span ID="button_span">
-            <?php if (isset($_SESSION['IS_CONNECTED'])==false) {
-            ?>
-            <form ID="log_in_button" action="log_in.html">
-                <button type="submit">Connexion</button>
-            </form>
-            <form ID="sign_up_button" action="sign_up.html">
-                <button type="submit">S'enregistrer</button>
-            </form>
-            <?php
-            }    
-            ?>
-            <?php if (isset($_SESSION['IS_CONNECTED'])) {
-            ?>
+
             <form ID="log_out_button" action="log_out.php">
                 <button type="submit">Déconnexion</button>
             </form>
-            <form ID="profile_button" action="profil/'<?php.$_SESSION['type_de_compte'].?>'.php">
+            <?php if ($_SESSION['type_de_compte'] == 'patient'){ ?>
+            <form ID="profile_button" action="profil/patient.php">
                 <button type="submit">Modifier votre profil</button>
             </form>
             <?php
-            }    
+            } if($_SESSION['type_de_compte'] == 'medecin') {   
+            ?>
+            <form ID="profile_button" action="profil/medecin.php">
+                <button type="submit">Modifier votre profil</button>
+            </form>
+            <?php
+            }
             ?>
         </span>
         <hr>
